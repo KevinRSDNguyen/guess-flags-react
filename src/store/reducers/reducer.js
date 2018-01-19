b9a02ea17c3848acf294e2e9af31f0df1f9f4657
kevin nguyen <kevinrsdnguyen@gmail.com>
2018-01-19 11:15:18 -0800

    Created a leaner switch case statement in the reducers file

--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -8,45 +8,54 @@ const initialState= {
   questionState: undefined  
 };
 
+const setCountries = (countries, state) => {
+  const correctOptionIndex = Math.floor(Math.random() * countries.length);
+  const flag = countries[correctOptionIndex].flag;
+  const correctOption = { name: countries[correctOptionIndex].name };
+  const options = getOptions(correctOption, countries);
+  return {
+    ...state,
+    countries,
+    options,
+    correctOption,
+    flag,
+    questionState: QuestionStates.QUESTION
+  };
+};
+
+const onGuess = (name, state) => {
+  const prevState = {...state};
+  const {correctOption} = prevState;
+  const questionState = name === correctOption.name ?
+    QuestionStates.ANSWER_CORRECT :
+    QuestionStates.ANSWER_WRONG;
+  return {
+    ...state,
+    questionState
+  };
+};
+
+const nextQuestion = (state) => {
+  const prevState = {...state};
+  const {countries} = prevState;
+  const correctOptionIndex = Math.floor(Math.random() * countries.length);
+  const flag = countries[correctOptionIndex].flag;
+  const correctOption = { name: countries[correctOptionIndex].name };
+  const options = getOptions(correctOption, countries);
+  return {
+    ...state,
+    options,
+    correctOption,
+    flag,
+    questionState: QuestionStates.QUESTION
+  };
+};
+
 const reducer = (state = initialState, action) => {
   switch(action.type) {
-    case 'SET_COUNTRIES':
-      const correctOptionIndex = Math.floor(Math.random() * action.countries.length);
-      const flag = action.countries[correctOptionIndex].flag;
-      const correctOption = { name: action.countries[correctOptionIndex].name };
-      const options = getOptions(correctOption, action.countries);
-      return {
-        ...state,
-        countries: action.countries,
-        options,
-        correctOption,
-        flag,
-        questionState: QuestionStates.QUESTION
-      };
-    case "ON_GUESS":
-      const prevState = {...state};
-      const {correctOption: correctOption3} = prevState;
-      const questionState = action.name === correctOption3.name ?
-        QuestionStates.ANSWER_CORRECT : 
-        QuestionStates.ANSWER_WRONG;
-      return {
-        ...state,
-        questionState
-      };
-    case "NEXT_QUESTION":
-      const prevState2 = {...state};
-      const {countries} = prevState2;
-      const correctOptionIndex2 = Math.floor(Math.random() * countries.length);
-      const flag2 = countries[correctOptionIndex2].flag;
-      const correctOption2 = { name: countries[correctOptionIndex2].name };
-      const options2 = getOptions(correctOption2, countries);
-      return {
-        ...state,
-        options: options2,
-        correctOption: correctOption2,
-        flag: flag2,
-        questionState: QuestionStates.QUESTION
-      };
+    case 'SET_COUNTRIES': return setCountries(action.countries, state);
+    case "ON_GUESS": return onGuess(action.name, state);
+    case "NEXT_QUESTION": return nextQuestion(state);
     default: return state;
   }
 };
